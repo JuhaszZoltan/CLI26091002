0969686dc18592b5deaf65a7e60a1b47121e85e2
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-16 11:55:30 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿class Person
+{
+    private string name;
+    private int numberOfChildren;
+
+    public string Name
+    {
+
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            if (value.Length == 0) throw new Exception("ADJ NEKI NEVET BASZOD!!!!!");
+            if (value == "Fasz") throw new Exception("SZÁPEN BESZÉLJÉL A KURVA ANYÁDAT!");
+
+            name = value;
+        }
+    }
+    //public int NumberOfChildren { get => numberOfChildren; set => numberOfChildren = value; }
+
+}
