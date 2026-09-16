4ddb1d251e8a08546e3af3aadcb197f98ea50880
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-16 13:55:45 +0200

    updt

--- Category.cs
+++ Category.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CLI26091002
+﻿class Category
 {
-    internal class Category
+    public string Name { get; set; }
+    public int Survivors { get; set; }
+    public int Missing { get; set; }
+
+    public override string ToString() => 
+        $"\tKategórianév:   {Name}\n" +
+        $"\tTúlélők száma:  {Survivors} fő\n" +
+        $"\tEltűntek száma: {Missing} fő";
+
+    public Category(string name, int survivors, int missing)
     {
-        public string Name { get; set; }
-        public int Survivors { get; set; }
-        public int Missing { get; set; }
+        Name = name;
+        Survivors = survivors;
+        Missing = missing;
+    }
 
-        public Category(string name, int survivors, int missing)
-        {
-            Name = name;
-            Survivors = survivors;
-            Missing = missing;
-        }
+    public Category(string row)
+    {
+        var tmp = row.Split(';');
 
-        public Category(string row)
-        {
-            var tmp = row.Split(';');
-
-            Name = tmp[0];
-            Survivors = int.Parse(tmp[1]);
-            Missing = int.Parse(tmp[2]);
-        }
+        Name = tmp[0];
+        Survivors = int.Parse(tmp[1]);
+        Missing = int.Parse(tmp[2]);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Text;
+
+const string TxtPath = "..\\..\\..\\data\\titanic.txt";
+
+List<Category> categories = [];
+using StreamReader sr = new(TxtPath, encoding: Encoding.UTF8);
+while (!sr.EndOfStream) categories.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"állományban található kategóriák száma: {categories.Count} db");
+
+var allPpl = categories.Sum(c => c.Survivors + c.Missing);
+Console.WriteLine($"az állományban szereplő személyek száma: {allPpl} fő");
+
+Console.Write("írja be a keresett kategórianév egy részét: ");
+string seachStr = Console.ReadLine() ?? throw new Exception("Ne hagyd üresen!");
+
+Console.WriteLine(
+    categories.Any(c => c.Name.Contains(seachStr)) 
+    ? "\tvan találat!"
+    : "\tnincs találat!");
